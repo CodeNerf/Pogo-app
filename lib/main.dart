6449e58a7cbc377a16d46498083285664715e776
Nick Maassen <nicholasmaassen@yahoo.com>
2023-01-30 15:59:09 -0500

    Moves amplify functions to their own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
-import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
-
-import 'amplifyconfiguration.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,18 +15,6 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     _configureAmplify();
-  }
-
-  Future<void> _configureAmplify() async {
-    try {
-      final auth = AmplifyAuthCognito();
-      await Amplify.addPlugin(auth);
-
-      // call Amplify.configure to use the initialized categories in your app
-      await Amplify.configure(amplifyconfig);
-    } on Exception catch (e) {
-      safePrint('An error occurred configuring Amplify: $e');
-    }
   }
 
   @override
