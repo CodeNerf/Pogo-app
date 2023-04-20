b10222c8ec5c2d52b52090474f2f01f3e9d12c80
dominic1515 <dgueccia@gmail.com>
2023-04-19 21:42:28 -0400

    added all issues definitions to class file for ease of use

--- lib/Onboarding/Issues.dart
+++ lib/Onboarding/Issues.dart
@@ -1,6 +1,7 @@
 import 'package:amplify_core/amplify_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:pogo/IssuesDefinitions.dart';
 import 'package:pogo/amplifyFunctions.dart';
 import '../HomeLoadingPage.dart';
 import '../dynamoModels/Demographics/UserDemographics.dart';
@@ -33,108 +34,19 @@ class Issues extends StatefulWidget {
 }
 
 class _IssuesState extends State<Issues> {
-  //TODO: fix text align/size in card
-
-  FixedExtentScrollController _extentScrollController =
-      FixedExtentScrollController();
-
   final String _pogoLogo = 'assets/Pogo_logo_horizontal.png';
   late int _issueIndex;
-  final List<String> _issuesLogo = [
-    'assets/gunPolicyPogo.jpeg',
-    'assets/climatePogo.jpg',
-    'assets/educationPogo.jpeg',
-    'assets/marijuana.png',
-    'assets/healthcarePogo.jpg',
-    'assets/housingPogo.jpg',
-    'assets/economyPogo.jpg',
-    'assets/immigrationPogo.jpg',
-    'assets/policingPogo.jpg',
-    'assets/reproductiveHealthPogo.jpg'
-  ];
-  final List<String> _issuesText = [
-    'GUN POLICY',
-    'CLIMATE CHANGE',
-    'EDUCATION',
-    'DRUG POLICY',
-    'HEALTHCARE',
-    'HOUSING',
-    'ECONOMY',
-    'IMMIGRATION',
-    'POLICING',
-    'REPRODUCTIVE RIGHTS'
-  ];
-  int _nextButtonColor = 0xFFF3D433;
-  final int _backgroundColor = 0xFFE1E1E1;
+
+  final int _nextButtonColor = 0xFFF3D433;
   double _alignRating = 0;
   double _valueRating = 0;
   late bool _backVisibility;
   late bool _forwardVisibility;
-  final Color _ratingBarColor = Colors.black;
   bool _submitVisibility = false;
   late String _submitButtonText;
   late double _submitButtonTextSize;
+  final IssuesDefinitions _issueDefs = IssuesDefinitions();
 
-  final List<String> _leftAlignText = [
-    'Gun Control',
-    'Acceptance',
-    'Public',
-    'Legalization',
-    'Government Funded',
-    'Affordable Housing',
-    'Market Regulation',
-    'Inclusive',
-    'Divestment & Reallocation',
-    'Abortion & Contraceptive Rights'
-  ];
-  final List<String> _rightAlignText = [
-    'Gun Rights',
-    'Doubt',
-    'School Choice',
-    'Criminalization',
-    'Private',
-    'Market Rate Housing',
-    'Market Deregulation',
-    'Exclusive',
-    'Investment',
-    'Abortion & Contraceptive Restrictions'
-  ];
-  final List<String> _issuesGeneralDefinitions = [
-    'Gun policy refers to the laws and protections in place around firearms. Concerning the possession, transfer, or sale of firearms or the expansion of background checks for firearm purchases.',
-    'Climate policy refers to rules to regulate innovation that affects the environment and the greater human race.',
-    'Education policy refers to the plan and underlying principles for educating students. The goals of educational policy have evolved in the United States as society and culture have changed, and are continually being debated and revised.',
-    'Drug policy refers to rules around the consumption, selling/and or purchasing of drugs or alcohol.',
-    'Healthcare policy refers to the concerns people have when it comes to “health freedom” / the freedom to choose. Some people believe that health and medical systems are heavily influenced by big government or pharmaceutical money therefore they want the space to self-determine and choose for oneself one’s course of treatment and approach to health overall.',
-    'Housing policy refers to the actions of the government, including legislation and program delivery, which have a direct or indirect impact on housing supply and availability, housing standards and urban planning.',
-    'Economic Policy refers to the systems for managing taxation, government budgets, the money supply and interest rates as well as the labor market, national ownership, and many other areas of government interventions into the economy.',
-    'Immigration policy refers to laws that control, protect, and manage the influx of people seeking to establish residence in the USA. It is rules regarding rights of access to the territory (entry and residence), permission to participate in the labor market (work permits), the rights of asylum seekers and refugees, the rights of immigrants to bring family members (family reunification), and rules for the acquisition of citizenship by immigrants and their family members (naturalization).',
-    'Policing policies refer to the foundation for all operations in law enforcement. Including training, budget management, consequences for excessive use of force, etc.',
-    'Reproductive Health policy refers to decisions and laws made around the reproductive system, its functions, and its processes.  It’s a state of complete physical, mental, and social well-being and not merely the absence of disease or infirmity.',
-  ];
-  final List<String> _issuesLeftDefinitions = [
-    'People in favor of gun control desire laws to be put in place such as background checks, wait times before buying a gun, banning automatic weapons, and disallowing concealed weapons',
-    'People in favor of climate policy are generally conservative in this area, preferring to ban economic activity that may create jobs but harm the environment.',
-    'People in favor of public education believe every child in America, regardless of family income or place of residence, deserves access to a quality education. Including: expanded, free, public education including free college; student-loan forgiveness, teacher-pay raises, and universal pre-kindergarten.',
-    'People who favor legalization believe that drug policy should be less strict including decreasing the penalties or punishments associated with the drug. Many people believe in the decriminalization of marijuana.',
-    'People in favor of government-funded healthcare believe that access to healthcare is a fundamental right for all people. They support “Universal Healthcare”, “The Affordable Care Act”, and the expansion of Medicare and Medicaid.',
-    'People in favor of Affordable housing believe that the government should support the creation of affordable housing and how it affects urban planning.',
-    'People in favor of market regulation desire for the economy to be run by a cooperative collective agency, which can mean the state but also a network of communes.',
-    'People in favor of inclusive immigration believe there should be pathways to citizenship for undocumented immigrants. Delay in deportations or prosecutions of undocumented immigrants who are young adults and have no criminal record.',
-    'People in favor of Divestment and Reallocation advocate for investments made in supportive services and divestment from policing institutions. They believe that money is invested into minority communities to criminalize them instead of supporting them systematically.',
-    'People in favor of “Pro-choice” generally believe in un-penalized access to abortion and both adult and embryonic stem cell research. They believe in “my body, my choice”.',
-  ];
-  final List<String> _issuesRightDefinitions = [
-    'People in favor of gun rights are strongly opposed to gun laws. Many are strong advocates of the second amendment [the right to bear arms], including “freedom to carry” for self-protection and relying on the state at little as possible.',
-    'People who doubt climate policy don’t believe the climate is a threat to our environment. They are more permissive when weighing the economic impact of environmental regulation. People who doubt climate change believe the free market will find its own solution to environmental issues.',
-    'People in favor of school choice believe academic performance, free speech, and federal and state separation are essential to a good education. They believe “keeping Washington out of education” to ensure parents are in control of what their kids are learning in their districts.',
-    'People who favor the criminalization of drugs believe that drug policy should be stricter including increasing the penalties or punishments associated with the drug.',
-    'People in favor of private healthcare believe there should be competition with Medicare from private insurance companies. They oppose “Universal Healthcare”, “The Affordable Care Act”, and Medicare expansion.',
-    'People in favor of Market rate housing believe that people should live where they can afford to and the government shouldn’t give tax breaks to support affordable housing.',
-    'People in favor of market deregulation desire for the economy to be left to the devices of competing individuals and organizations.',
-    'No “amnesty” for undocumented immigrants; stronger border patrol, etc. There’s a strong belief that illegal immigration is lowering the wages for citizens and documented immigrants.',
-    'People in favor of Investment advocate for more funding for training, weaponry, and local policing infrastructure.',
-    'People in favor of abortion and contraceptive criminalization believe that people shouldn’t get abortions or use contraceptives no matter what. They believe that a baby is alive at the moment of conception. Abortions/abortion relation surgeries and contraceptives should be criminalized.',
-  ];
   @override
   void initState() {
     super.initState();
@@ -500,7 +412,7 @@ class _IssuesState extends State<Issues> {
                                   ),
                                   child: Image(
                                     image: AssetImage(
-                                      _issuesLogo[_issueIndex],
+                                      _issueDefs.issuesLogo[_issueIndex],
                                     ),
                                     fit: BoxFit.fill,
                                   ),
@@ -546,7 +458,7 @@ class _IssuesState extends State<Issues> {
                                                   Padding(
                                                     padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
                                                     child: Text(
-                                                      _issuesText[_issueIndex],
+                                                      _issueDefs.issuesText[_issueIndex],
                                                       style: const TextStyle(
                                                         fontFamily: 'Inter',
                                                         fontWeight: FontWeight.w600,
@@ -581,7 +493,7 @@ class _IssuesState extends State<Issues> {
                                                               ),
                                                               child: Image(
                                                                 image: AssetImage(
-                                                                  _issuesLogo[_issueIndex],
+                                                                  _issueDefs.issuesLogo[_issueIndex],
                                                                 ),
                                                                 fit: BoxFit.fill,
                                                               ),
@@ -591,7 +503,7 @@ class _IssuesState extends State<Issues> {
                                                           Padding(
                                                             padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
                                                             child: Text(
-                                                              _issuesGeneralDefinitions[_issueIndex],
+                                                              _issueDefs.issuesGeneralDefinitions[_issueIndex],
                                                               style: const TextStyle(
                                                                 fontFamily: 'Inter',
                                                                 fontWeight: FontWeight.w500,
@@ -606,7 +518,7 @@ class _IssuesState extends State<Issues> {
                                                             child: Align(
                                                               alignment: Alignment.centerLeft,
                                                               child: Text(
-                                                                '• ${_leftAlignText[_issueIndex]}',
+                                                                '• ${_issueDefs.leftAlignText[_issueIndex]}',
                                                                 style: const TextStyle(
                                                                   fontFamily: 'Inter',
                                                                   fontWeight: FontWeight.w500,
@@ -620,7 +532,7 @@ class _IssuesState extends State<Issues> {
                                                           Padding(
                                                             padding: const EdgeInsets.fromLTRB(35, 10, 35, 0),
                                                             child: Text(
-                                                              '▪ ${_issuesLeftDefinitions[_issueIndex]}',
+                                                              '▪ ${_issueDefs.issuesLeftDefinitions[_issueIndex]}',
                                                               style: const TextStyle(
                                                                 fontFamily: 'Inter',
                                                                 fontWeight: FontWeight.w500,
@@ -635,7 +547,7 @@ class _IssuesState extends State<Issues> {
                                                             child: Align(
                                                               alignment: Alignment.centerLeft,
                                                               child: Text(
-                                                                '• ${_rightAlignText[_issueIndex]}',
+                                                                '• ${_issueDefs.rightAlignText[_issueIndex]}',
                                                                 style: const TextStyle(
                                                                   fontFamily: 'Inter',
                                                                   fontWeight: FontWeight.w500,
@@ -649,7 +561,7 @@ class _IssuesState extends State<Issues> {
                                                           Padding(
                                                             padding: const EdgeInsets.fromLTRB(35, 10, 35, 20),
                                                             child: Text(
-                                                              '▪ ${_issuesRightDefinitions[_issueIndex]}',
+                                                              '▪ ${_issueDefs.issuesRightDefinitions[_issueIndex]}',
                                                               style: const TextStyle(
                                                                 fontFamily: 'Inter',
                                                                 fontWeight: FontWeight.w500,
@@ -678,7 +590,7 @@ class _IssuesState extends State<Issues> {
                         Expanded(
                           flex: 6,
                           child: AutoSizeText(
-                            _issuesText[_issueIndex],
+                            _issueDefs.issuesText[_issueIndex],
                             maxLines: 1,
                             style: const TextStyle(
                               fontFamily: 'Inter',
@@ -734,7 +646,7 @@ class _IssuesState extends State<Issues> {
                                       0.65 /
                                       3,
                                   child: AutoSizeText(
-                                    _leftAlignText[_issueIndex],
+                                    _issueDefs.leftAlignText[_issueIndex],
                                     maxLines: 3,
                                     textAlign: TextAlign.start,
                                     style: const TextStyle(
@@ -748,7 +660,7 @@ class _IssuesState extends State<Issues> {
                                       0.65 /
                                       2,
                                   child: AutoSizeText(
-                                    _rightAlignText[_issueIndex],
+                                    _issueDefs.rightAlignText[_issueIndex],
                                     maxLines: 3,
                                     textAlign: TextAlign.end,
                                     style: const TextStyle(
