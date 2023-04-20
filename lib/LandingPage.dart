2bc1f54d2e20a46fecfb0a06f61c1424dd79000f
dominic1515 <dgueccia@gmail.com>
2023-04-19 21:44:56 -0400

    added constant declarations to landingpage, removed unused imports

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -1,11 +1,6 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 import 'AppWalkThrough.dart';
 import 'SignInSignUpPage.dart';
-import 'amplifyFunctions.dart';
-import 'Onboarding/AddressAutocomplete.dart';
-import 'dart:math';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -18,12 +13,12 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFF1F4F8),
+      backgroundColor: const Color(0xFFF1F4F8),
       body: SafeArea(
         child: Container(
           width: double.infinity,
           height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -33,15 +28,15 @@ class _LandingPageState extends State<LandingPage> {
                   Container(
                     width: 599,
                     height: 212,
-                    margin: EdgeInsets.only(left: 5, top: 30, right: 5),
-                    decoration: BoxDecoration(
+                    margin: const EdgeInsets.only(left: 5, top: 30, right: 5),
+                    decoration: const BoxDecoration(
                       image: DecorationImage(
                         image: AssetImage("assets/Pogo_logo_horizontal.png"),
                         alignment: Alignment.center,
                       ),
                     ),
                   ),
-                  Align(
+                  const Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
                       "Politics on the Go",
@@ -56,10 +51,10 @@ class _LandingPageState extends State<LandingPage> {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
-                  Align(
+                  const Align(
                     alignment: Alignment.centerLeft,
                     child: Text(
                       "Informing the next generation of voters ",
@@ -76,7 +71,7 @@ class _LandingPageState extends State<LandingPage> {
                   ),
                 ],
               ),
-              SizedBox(height: 40),
+              const SizedBox(height: 40),
               Column(
                 // ignore: sort_child_properties_last
                 children: <Widget>[
@@ -89,7 +84,7 @@ class _LandingPageState extends State<LandingPage> {
                         BoxShadow(
                           color: Colors.black.withOpacity(0.35),
                           blurRadius: 4,
-                          offset: Offset(0, 4),
+                          offset: const Offset(0, 4),
                         ),
                       ],
                     ),
@@ -106,7 +101,7 @@ class _LandingPageState extends State<LandingPage> {
                       },
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)),
-                      child: Text(
+                      child: const Text(
                         "Login",
                         style: TextStyle(
                           fontFamily: 'Inter',
@@ -122,13 +117,13 @@ class _LandingPageState extends State<LandingPage> {
                   const SizedBox(height: 40),
                   Container(
                     decoration: BoxDecoration(
-                      color: Color(0xFFF3D433),
+                      color: const Color(0xFFF3D433),
                       borderRadius: BorderRadius.circular(12),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(0.35),
                           blurRadius: 4,
-                          offset: Offset(0, 4),
+                          offset: const Offset(0, 4),
                         ),
                       ],
                     ),
@@ -140,11 +135,11 @@ class _LandingPageState extends State<LandingPage> {
                             context,
                             MaterialPageRoute(
                                 builder: (context) =>
-                                    AppWalkThrough()));
+                                    const AppWalkThrough()));
                       },
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(10)),
-                      child: Text(
+                      child: const Text(
                         "Sign Up",
                         style: TextStyle(
                           fontFamily: 'Inter',
